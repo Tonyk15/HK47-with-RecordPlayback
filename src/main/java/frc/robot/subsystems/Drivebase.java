2c5054a2075867bbf2546b18f44985bb7ce4e487
Tony <45805257+MarshallVielmetti@users.noreply.github.com>
2021-12-02 20:48:25 -0600

    initial commit

---
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants;
+
+
+public class Drivebase extends SubsystemBase {
+  final CANSparkMax frontLeftMotor;
+  final CANSparkMax rearLeftMotor;
+  final CANSparkMax frontRightMotor;
+  final CANSparkMax rearRightMotor;
+  
+  /** Creates a new Drivebase. */
+  public Drivebase() {
+    frontLeftMotor = new CANSparkMax(Constants.frontLeftID, MotorType.kBrushless);
+    rearLeftMotor = new CANSparkMax(Constants.rearLeftID, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(Constants.frontRightID, MotorType.kBrushless);
+    rearRightMotor = new CANSparkMax(Constants.rearRightID, MotorType.kBrushless);
+
+    // LEFT OFF HERE
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
